e9aa9c764e899ee107e2f8de4888f622e00ea78c
Xavier Didelot <xavier.didelot@gmail.com>
2024-12-10 12:13:49 +0000

    figures

--- run/figureNegBin.R
+++ run/figureNegBin.R
@@ -15,7 +15,8 @@ plot1<-ggplot(data, aes(x=x)) + scale_x_continuous(breaks=seq(0,10,1)) +theme(pa
   xlab('Offspring number')+ylab('Probability')
 
 
-nt=10
+nt=20
+n=10
 data=data.frame()
 v=sapply(1:10, function(k) pois_inclusive(k,nt=nt))
 data=rbind(data,data.frame(x=1:10,y=v,Distribution='Poisson'))
@@ -25,20 +26,20 @@ for (i in 1:length(rs)) {
 data$Distribution=factor(data$Distribution,levels=unique(data$Distribution))#forces order to remain as input
 plot2<-ggplot(data, aes(x=x)) + scale_x_continuous(breaks=seq(0,10,1)) +theme(panel.grid.minor.x = element_blank() )+
   geom_line(aes(y = y, colour = Distribution)) + geom_point(aes(y=y,colour=Distribution))+
-  xlab('Size of multimerger event')+ylab('Inclusive Probability') + scale_y_log10(breaks=10^-(0:10),limits=c(1e-10,1))+theme(panel.grid.minor.y = element_blank() )
+  xlab('Size of multimerger event')+ylab('Inclusive Probability') + scale_y_log10(breaks=10^-(0:12),limits=c(1e-12,1))+theme(panel.grid.minor.y = element_blank() )
 
 data=data.frame()
-v=sapply(1:10, function(k) pois_exclusive(k,n=15,nt=nt))
+v=sapply(1:10, function(k) pois_exclusive(k=k,n=n,nt=nt))
 data=rbind(data,data.frame(x=1:10,y=v,Distribution='Poisson'))
 for (i in 1:length(rs)) {
-  v=sapply(1:10, function(k) negbin_exclusive(k,n=15,nt=nt,r=rs[i]))
+  v=sapply(1:10, function(k) negbin_exclusive(k=k,n=n,nt=nt,r=rs[i]))
   data=rbind(data,data.frame(x=1:10,y=v,Distribution=paste0('NegBin(r=',rs[i],')')))}
 data$Distribution=factor(data$Distribution,levels=unique(data$Distribution))#forces order to remain as input
 plot3<-ggplot(data, aes(x=x)) + scale_x_continuous(breaks=seq(0,10,1)) +theme(panel.grid.minor.x = element_blank() )+
   geom_line(aes(y = y, colour = Distribution)) + geom_point(aes(y=y,colour=Distribution))+
-  xlab('Size of multimerger event')+ylab('Exclusive Probability') + scale_y_log10(breaks=10^-(0:10),limits=c(1e-10,1))+theme(panel.grid.minor.y = element_blank() )
+  xlab('Size of multimerger event')+ylab('Exclusive Probability') + scale_y_log10(breaks=10^-(0:12),limits=c(1e-12,1))+theme(panel.grid.minor.y = element_blank() )
 
-pdf('figure.pdf',7,7)
+pdf('figureNegBin.pdf',7,7)
 plot1+plot2+plot3+guide_area()+plot_layout(ncol = 2, nrow = 2, guides = "collect")+plot_annotation(tag_levels = 'A')
 dev.off()
-system('open figure.pdf')
+system('open figureNegBin.pdf')
---
+++ run/figurePois.R
@@ -0,0 +1,30 @@
+rm(list=ls())
+library(EpiLambda)
+library(ggplot2)
+library(patchwork)
+
+rt=1
+data=data.frame()
+v=sapply(1:10, function(k) pois_inclusive(k,nt=10))
+data=rbind(data,data.frame(x=1:10,y=v,PopSize='10',Type='Inclusive'))
+v=sapply(1:10, function(k) pois_exclusive(k,n=10,nt=10))
+data=rbind(data,data.frame(x=1:10,y=v,PopSize='10',Type='Exclusive'))
+v=sapply(1:10, function(k) pois_inclusive(k,nt=20))
+data=rbind(data,data.frame(x=1:10,y=v,PopSize='20',Type='Inclusive'))
+v=sapply(1:10, function(k) pois_exclusive(k,n=10,nt=20))
+data=rbind(data,data.frame(x=1:10,y=v,PopSize='20',Type='Exclusive'))
+v=sapply(1:10, function(k) pois_inclusive(k,nt=30))
+data=rbind(data,data.frame(x=1:10,y=v,PopSize='30',Type='Inclusive'))
+v=sapply(1:10, function(k) pois_exclusive(k,n=10,nt=30))
+data=rbind(data,data.frame(x=1:10,y=v,PopSize='30',Type='Exclusive'))
+data$Type=factor(data$Type,levels=unique(data$Type))#forces order to remain as input
+data$PopSize=factor(data$PopSize,levels=unique(data$PopSize))#forces order to remain as input
+plot1<-ggplot(data, aes(x=x)) + scale_x_continuous(breaks=seq(0,10,1)) +theme(panel.grid.minor.x = element_blank() )+
+  geom_line(aes(y = y, colour = PopSize,group=interaction(PopSize,Type))) + geom_point(aes(y=y,colour=PopSize,shape=Type))+
+  xlab('Size of multimerger event')+ylab('Probability') + scale_y_log10(breaks=10^-(0:15),limits=c(1e-15,1))+theme(panel.grid.minor.y = element_blank() )
+
+
+pdf('figurePois.pdf',7,7)
+plot1
+dev.off()
+system('open figurePois.pdf')
