e09ebf0a17316bc9108e3bb0a90c2fd64261e145
Xavier Didelot <xavier.didelot@gmail.com>
2024-12-10 14:19:49 +0000

    More trees

--- run/figureTree.R
+++ run/figureTree.R
@@ -1,10 +1,17 @@
 library(EpiLambda)
 library(ggplot2)
 library(ggtree)
+library(patchwork)
 set.seed(0)
-t=new_simtree(n=20,nt=40,r=0.5)
-pdf('figureTree.pdf')
-p <- ggtree(t) + theme_tree2() + scale_x_continuous(labels = abs)+ geom_tiplab()+xlab("Generations since present")
-revts(p)
+pdf('figureTree.pdf',10,10)
+ps=list()
+rs=c(0.1,1,5,10)
+for (i in 1:4) {
+  t=new_simtree(n=20,nt=100,r=rs[i])
+  p <- ggtree(t) + theme_tree2() + scale_x_continuous(labels = abs)+ geom_tiplab()+xlab("Generations since present")
+  p = revts(p)
+  ps[[i]]=p
+}
+ps[[1]]+ps[[2]]+ps[[3]]+ps[[4]]+plot_layout(ncol = 2, nrow = 2, guides = "collect")+plot_annotation(tag_levels = 'A')
 dev.off()
 system('open figureTree.pdf')
