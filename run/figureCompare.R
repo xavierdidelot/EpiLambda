2f19898c5974836ef36c6a66b046f8fb91240e88
Xavier Didelot <xavier.didelot@gmail.com>
2025-03-19 14:27:20 +0000

    revisions

--- run/figureCompare.R
+++ run/figureCompare.R
@@ -29,6 +29,10 @@ data$alpha=factor(data$alpha,levels=unique(data$alpha))#forces order to remain a
 pdf('figureCompare.pdf')
 #ggplot(data, aes(k,p,fill=alpha))+geom_bar(stat="identity",position='dodge')+ scale_x_continuous(breaks=seq(2,n,1))+xlab('Size of next event')+ylab('Probability of event')
 ggplot(data, aes(k, alpha, fill=Probability)) + geom_tile()+scale_fill_viridis(limits=limits,breaks=limits[1]:limits[2],labels=c('<0.0001','0.001','0.01','0.1','1'))+
-  scale_x_continuous(breaks=seq(2,n,1),expand = c(0, 0))+scale_y_discrete(expand = c(0, 0))+xlab('Size of next event')+ylab('')
+  scale_x_continuous(breaks=seq(2,n,1),expand = c(0, 0))+scale_y_discrete(expand = c(0, 0))+xlab('Size of next event')+ylab('')+
+  geom_segment(aes(x = -3, xend = 10.5, y = 3.5, yend = 3.5), linetype="dashed", color = "red")+
+  geom_segment(aes(x = -3, xend = 10.5, y = 6.5, yend = 6.5), linetype="dashed", color = "red")+
+  geom_segment(aes(x = -3, xend = 10.5, y = 9.5, yend = 9.5), linetype="dashed", color = "red")+
+  coord_cartesian(xlim = c(0, 10.5), clip="off")
 dev.off()
 system('open figureCompare.pdf')
